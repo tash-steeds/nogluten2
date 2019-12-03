a331a55d3dcfcd5a6bd7b437f18e6ddf30bccdc1
youngyoonc <56639642+youngyoonc@users.noreply.github.com>
2019-12-03 10:40:55 +0900

    sticky navbar
    bug fixed!

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  window.addEventListener('scroll', myFunctionForSticky);
+window.addEventListener('scroll', myFunctionForSticky);
+//window.onscroll = function() {myFunctionForSticky()};
 
-  var navbar = document.getElementById("navbar");
-
-  var sticky = navbar.offsetTop;
+  // var navbar = document.getElementById("navbar");
+  var navbar = document.getElementsByClassName("navbar");
+  var sticky = navbar[0].offsetTop;
 
   function myFunctionForSticky() {
     if (window.pageYOffset >= sticky) {
       console.log("window.pageYOffset >= sticky");
+      navbar[0].classList.add("sticky")
     } else {
       console.log("Not window.pageYOffset >= sticky");
+      navbar[0].classList.remove("sticky");
     }
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky");
-    } else {
-      navbar.classList.remove("sticky");
-    }
+    // if (window.pageYOffset >= sticky) {
+    //   navbar.classList.add("sticky");
+    // } else {
+    //   navbar.classList.remove("sticky");
+    // }
   }
 
   function myFunctionForResponsive() {
-    navbar.classList.toggle('responsive');
+    navbar[0].classList.toggle('responsive');
   } 
 })
